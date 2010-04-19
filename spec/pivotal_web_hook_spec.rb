e384477c815a0f38647f40b807f80fb8fe87d126
gustin <gustin@entryway.net>
2010-04-18 23:52:24 -0500

    Extract to module
    Moved the xpath functionallity to the Pivotal::Api module

--- spec/pivotal_web_hook_spec.rb
+++ spec/pivotal_web_hook_spec.rb
@@ -4,17 +4,17 @@ describe 'Pivotal activity hook' do
 
   context 'reading xml' do
     context 'status: started' do
-        subject { PivotalProgress.new(xml('started')) }
+        subject { Pivotal::Progress.new(xml('started')) }
         its(:state) { should == 'started' }
     end
 
     context 'status: unstarted' do 
-      subject { PivotalProgress.new(xml('unstarted')) }
+      subject { Pivotal::Progress.new(xml('unstarted')) }
       its(:state) { should == 'unstarted' }
     end
 
     context 'status: finshed' do 
-      subject { PivotalProgress.new(xml('finished')) }
+      subject { Pivotal::Progress.new(xml('finished')) }
       its(:state) { should == 'finished' }
     end
   end
@@ -38,6 +38,69 @@ describe 'Pivotal activity hook' do
         </stories>
       </activity>}
   end
+
+  context 'obtains project name from tracker' do
+    subject { Pivotal::Project.new(project(0000, 'Foo Bar')) }
+    its(:name) { should == 'Foo Bar' }
+
+    def project(id, name)
+      %Q{<?xml version="1.0" encoding="UTF-8"?>
+          <project>
+          <id>#{id}</id>
+          <name>#{name}</name>
+          <iteration_length type="integer">2</iteration_length>
+          <week_start_day>Monday</week_start_day>
+          <point_scale>0,1,2,3</point_scale>
+          <account>James Kirks Account</account>
+          <velocity_scheme>Average of 4 iterations</velocity_scheme>
+          <current_velocity>10</current_velocity>
+          <initial_velocity>10</initial_velocity>
+          <number_of_done_iterations_to_show>12</number_of_done_iterations_to_show>
+          <labels>shields,transporter</labels>
+          <allow_attachments>true</allow_attachments>
+          <public>false</public>
+          <use_https>true</use_https>
+          <bugs_and_chores_are_estimatable>false</bugs_and_chores_are_estimatable>
+          <commit_mode>false</commit_mode>
+          <last_activity_at type="datetime">2010/01/16 17:39:10 CST</last_activity_at>
+          <memberships>
+            <membership>
+              <id>1006</id>
+              <person>
+                <email>kirkybaby@earth.ufp</email>
+                <name>James T. Kirk</name>
+                <initials>JTK</initials>
+              </person>
+              <role>Owner</role>
+            </membership>
+          </memberships>
+          <integrations>
+            <integration>
+              <id type="integer">2</id>
+              <type>Lighthouse</type>
+              <name>Lighthouse Feature Bin</name>
+              <field_name>lighthouse_id</field_name>
+              <field_label>Lighthouse Id</field_label>
+              <active>true</active>
+            </integration>
+            <integration>
+              <id type="integer">2</id>
+              <type>Lighthouse</type>
+              <name>Lighthouse Bug Bin</name>
+              <field_name>lighthouse_id</field_name>
+              <field_label>Lighthouse Id</field_label>
+              <active>false</active>
+            </integration>
+            <integration>
+              <id type="integer">3</id>
+              <type>Other</type>
+              <name>United Federation of Planets Bug Tracker</name>
+              <field_name>other_id</field_name>
+              <field_label>United Federation of Planets Bug Tracker Id</field_label>
+              <active>true</active>
+            </integration>
+          </integrations>
+        </project>}
+    end
+  end
 end
-
-
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,9 @@ require File.join(File.dirname(__FILE__), '..', 'server.rb')
 require File.join(File.dirname(__FILE__), '..', 'client.rb')
 require File.join(File.dirname(__FILE__), '..', 'project.rb')
 require File.join(File.dirname(__FILE__), '..', 'progress_report.rb')
-require File.join(File.dirname(__FILE__), '..', 'plugins', 'pivotal_tracker', 'pivotal_progress.rb')
+require File.join(File.dirname(__FILE__), '..', 'plugins', 'pivotal_tracker', 'api.rb')
+require File.join(File.dirname(__FILE__), '..', 'plugins', 'pivotal_tracker', 'progress.rb')
+require File.join(File.dirname(__FILE__), '..', 'plugins', 'pivotal_tracker', 'project.rb')
 
 require 'sinatra'
 require 'rack/test'
